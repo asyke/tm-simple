c724a9bac169d7bf194b8270de6b175b69bbd8d5
Asylbek Ibrakhimov <asylbek.ibrahimov@gmail.com>
2026-03-26 20:41:37 -0500

    fix: update build step conditions to allow specific feature branches

--- scripts/vercel/ignore-build-step.sh
+++ scripts/vercel/ignore-build-step.sh
@@ -4,7 +4,7 @@ BRANCH="${VERCEL_GIT_COMMIT_REF:-${VERCEL_GIT_BRANCH:-}}"
 
 echo "Branch is: $BRANCH"
 
-if [[ "$BRANCH" == "vercel" ]]; then
+if [[ "$BRANCH" == "vercel" || "$BRANCH" == "feature/test-preview" || "$BRANCH" == feature/test-preview-* ]]; then
   echo "✅ Build allowed"
   exit 1
 else
